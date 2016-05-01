7767cf72f9125071f689519d301b0ff8a187e13d
Kavi Gupta <kavi@globalmbox.com>
2016-05-01 15:18:46 -0700

    AD now works for fractionals too. Also have a higher-derivative engine

--- AD.hs
+++ AD.hs
@@ -3,5 +3,19 @@ module AD () where
 
 import Dual
 
-dNum :: (forall a. (Num a, Eq a) => a -> a) -> (forall a. (Num a, Eq a) => a -> a)
+type PolyNum = forall a. (Num a, Eq a) => a -> a
+type PolyFrac = forall a. (Fractional a, Eq a) => a -> a
+
+dNum :: PolyNum -> PolyNum
 dNum f x = du $ f (dual x 1)
+
+dNum' :: (Integral a) => a -> PolyNum -> PolyNum
+dNum' 0 f = f
+dNum' n f = dNum' (n-1) $ dNum f
+
+dFrac :: PolyFrac -> PolyFrac
+dFrac f x = du $ f (dual x 1)
+
+dFrac' :: (Integral a) => a -> PolyFrac -> PolyFrac
+dFrac' 0 f = f
+dFrac' n f = dFrac' (n-1) $ dFrac f
