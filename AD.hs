66a32f8b05489c303c76d4d360f8ff498e9a789e
Kavi Gupta <kavi@globalmbox.com>
2016-05-01 17:03:40 -0700

    Added AD for floats

--- AD.hs
+++ AD.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE RankNTypes #-}
-module AD () where
+module AD (dNum, dNum', dFrac, dFrac', dFloat, dFloat') where
 
 import Dual
 
 type PolyNum = forall a. (Num a, Eq a) => a -> a
 type PolyFrac = forall a. (Fractional a, Eq a) => a -> a
+type PolyFloat = forall a. (Floating a, Eq a) => a -> a
 
 dNum :: PolyNum -> PolyNum
 dNum f x = du $ f (dual x 1)
@@ -19,3 +20,10 @@ dFrac f x = du $ f (dual x 1)
 dFrac' :: (Integral a) => a -> PolyFrac -> PolyFrac
 dFrac' 0 f = f
 dFrac' n f = dFrac' (n-1) $ dFrac f
+
+dFloat :: PolyFloat -> PolyFloat
+dFloat f x = du $ f (dual x 1)
+
+dFloat' :: (Integral a) => a -> PolyFloat -> PolyFloat
+dFloat' 0 f = f
+dFloat' n f = dFloat' (n-1) $ dFloat f
--- Dual.hs
+++ Dual.hs
@@ -38,5 +38,28 @@ instance (Eq x, Fractional x) => Fractional (Dual x) where
     recip (D a b) = D (1 / a) (-b / (a * a))
     fromRational x = D (fromRational x) 0
 
+lift :: (Num a) => (a -> a) -> (a -> a) -> (Dual a -> Dual a)
+lift f f' (D a b) = D (f a) (b * f' a)
+
+instance (Eq x, Floating x) => Floating (Dual x) where
+    pi = D pi 0
+    exp = lift exp exp
+    log = lift log (abs . (1/))
+    sqrt = lift sqrt (\x -> 1 / (2 * sqrt x))
+    a ** b = exp $ b * log a
+    logBase a b = log b / log a
+    sin = lift sin cos
+    cos = lift cos (negate . sin)
+    tan = lift tan (\x -> 1 / (cos x * cos x))
+    asin = lift asin (\x -> 1 / sqrt (1 - x * x))
+    acos = lift acos (\x -> -1 / sqrt (1 - x * x))
+    atan = lift atan (\x -> 1 / (1 + x * x))
+    sinh = lift sinh cosh
+    cosh = lift cosh sinh
+    tanh = lift tanh (\x -> 1 - tanh x * tanh x)
+    asinh = lift asinh (\x -> 1 / sqrt (x * x + 1))
+    acosh = lift acosh (\x -> 1 / sqrt (x * x - 1))
+    atanh = lift atanh (\x -> 1 / (1 - x * x))
+
 eps :: (Num a) => Dual a
 eps = D 0 1
